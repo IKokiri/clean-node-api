aa1f6789ac9093d70a9c6b55cd5f837659145367
Luiz Mendes <luiz.mendes@in8.com.br>
2021-08-28 03:46:00 -0300

    refactor: add customized error for mussing param

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,4 +1,6 @@
 import { SignUpController } from './signup'
+import { MissingParamError } from '../errors/missing-param-error'
+
 describe('SingUp Controller', () => {
   test('Should return if no name is provided', () => {
     const sut = new SignUpController()
@@ -11,7 +13,7 @@ describe('SingUp Controller', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+    expect(httpResponse.body).toEqual(new MissingParamError('name'))
   })
 
   test('Should return if no email is provided', () => {
@@ -25,7 +27,6 @@ describe('SingUp Controller', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+    expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
 })
-
